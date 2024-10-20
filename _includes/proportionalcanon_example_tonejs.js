d4a7f512e217b22cadd7a7d4e54ab0bcc23a8039
kumezaki <kojiroumezaki@gmail.com>
2024-10-20 11:22:10 -0400

    updated proportionalcanon_example_tonejs.js to make play and stop work

--- _includes/proportionalcanon_example_tonejs.js
+++ _includes/proportionalcanon_example_tonejs.js
@@ -12,36 +12,69 @@ let synths_array = null;
 let filters_array = null;
 let gains_array = null;
 
-function create_arrays()
+function create_arrays(block_index)
 {
 	synths_array = new Array(num_blocks).fill(null);
+	
 	filters_array = new Array(num_blocks).fill(null);
+	
 	gains_array = new Array(num_blocks).fill(null);
 }
 
-function create_block(block_index)
+function init_arrays(block_index)
 {
-	let melody = melodies[block_index];
-
-	let start_time = block_index / num_blocks * dur_block;
-	
+	// synths
 	synths_array[block_index] = Array.from({ length: num_block_parts }, () => new Tone.Synth({
 		oscillator: { type: 'sawtooth' },
 		envelope: { attack: 0, decay: 0, sustain: 1, release: 0 }
 	}).set({ volume: synth_volume }));
+
+	let melody = melodies[block_index];	
+
+	const noteNamesArray = Array.from({ length: num_block_parts },
+		(_, index) => melody.map(midi => Tone.Frequency(midi+((index+max_8vb)*12), "midi").toNote()));
+
+	const dur_note = dur_block / melody.length; // seconds
 	
+	const start_time_offset = block_index / num_blocks * dur_block;
+
+	const seqs = Array.from({ length: num_block_parts }, (_, index) => new Tone.Sequence((time, note) => {
+		synths_array[block_index][index].triggerAttackRelease(note, dur_note/(index+1), time);
+	}, noteNamesArray[index], dur_note/(index+1)).start(start_time_offset));
+	
+	seqs.forEach((seq, index) => { seq.loop = index + 1; }); // number of loop iterations
+
+	// filters
 	filters_array[block_index] = Array.from({ length: num_block_parts }, () => new Tone.Filter({
 		type: 'lowpass',
 		frequency: 4000,
 		rolloff: -12
 	}));
-	
-	gains_array[block_index] = Array.from({ length: num_block_parts }, () => new Tone.Gain(1).toDestination());
+
+	// gains
+	gains_array[block_index] = Array.from({ length: num_block_parts }, () => new Tone.Gain(1));
+
+	// connect synths to filters to gains to output
+	synths_array[block_index].forEach((synth, index) => {
+		synth.connect(filters_array[block_index][index]);
+		filters_array[block_index][index].connect(gains_array[block_index][index]);
+		gains_array[block_index][index].toDestination();
+	});
+}
+
+function init_gains_events(block_index, start_time)
+{
+	const start_time_offset = block_index / num_blocks * dur_block;
+
 	gains_array[block_index].forEach((gain, index) => {
+		// cancel existing ramps
+		gain.gain.cancelScheduledValues(start_time);
+
+		// add new ramps
 		const silent_time_interval = dur_block * 0.02 // seconds; 2%
 		const silent_time = index * silent_time_interval;
 		const fade_time = dur_block / 2 - silent_time;
-		let t = start_time;
+		let t = start_time + start_time_offset; // gain time events are based on Tone.now(), not transport
 		gain.gain.setValueAtTime(0, t); // Start at 0 gain
 		t += silent_time;
 		gain.gain.linearRampToValueAtTime(0, t); // Maintain silence
@@ -50,45 +83,23 @@ function create_block(block_index)
 		t += fade_time;
 		gain.gain.linearRampToValueAtTime(0, t); // Fade out
 	});
-	
-	// Connect synths to filters and gains
-	synths_array[block_index].forEach((synth, index) => {
-		synth.connect(filters_array[block_index][index]);
-		filters_array[block_index][index].connect(gains_array[block_index][index]);
-		gains_array[block_index][index].toDestination();
-	});
-	
-	const max_dur_note = dur_block / melody.length; // seconds
-	
-	const noteNamesArray = Array.from({ length: num_block_parts },
-		(_, index) => melody.map(midi => Tone.Frequency(midi+((index+max_8vb)*12), "midi").toNote()));
-	
-	const dur = max_dur_note;
-	
-	const seqs = Array.from({ length: num_block_parts }, (_, index) => new Tone.Sequence((time, note) => {
-		synths_array[block_index][index].triggerAttackRelease(note, dur/(index+1), time);
-	}, noteNamesArray[index], dur/(index+1)).start(start_time));
-	
-	seqs.forEach((seq, index) => { seq.loop = index + 1; });
 }
 
 function play() {
-	console.log("play() called");
-
-	create_arrays();
-	synths_array.forEach((synths, block_index) => {
-		create_block(block_index);
+	gains_array.forEach((gains, block_index) => {
+		init_gains_events(block_index,Tone.now());
 	});
 	
 	Tone.Transport.start();
 }
 
 function stop() {
-	console.log("stop() called");
-
 	synths_array.forEach((synths, block_index) => {
 		synths.forEach(synth => { synth.triggerRelease(); });
 	});
 	
 	Tone.Transport.stop();
 }
+
+create_arrays();
+synths_array.forEach((synths, block_index) => { init_arrays(block_index); });
